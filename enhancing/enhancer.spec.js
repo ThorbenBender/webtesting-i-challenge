54108fc207746cff29a2b1baf0bb7f545e0218ce
Thorben Bender <bender.thorben0@gmail.com>
2019-04-08 15:00:52 +0200

    test for repair is done and repair function is working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('helpers module', () => {
+	describe('repair function', () => {
+		const item = { name: 'Sword', durability: 5, enhancement: 2 };
+		it('should have 100 durability', () => {
+			expect(enhancer.repair(item)).toEqual({ durability: 100, ...item });
+		});
+	});
+});
